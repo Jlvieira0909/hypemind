299eb1ee8367709e35629bd4ae960018a843ba10
Jlvieira0909 <lvjohn09@gmail.com>
2025-01-09 22:57:31 -0300

    "added a lot of sutff"

--- src/Components/ProjectContainer/ProjectContainer.js
+++ src/Components/ProjectContainer/ProjectContainer.js
@@ -1,19 +1,28 @@
-import { Link } from 'react-router-dom'
-import './ProjectContainer.css'
+import { Link } from "react-router-dom";
+import "./ProjectContainer.css";
 
 const ProjectContainer = (props) => {
-    return(
-        <Link href = "/projects" className="ProjectLabel" target="_blank" rel="noopener noreferrer">
-            <div className="ProjectsWrapper">
-                <h2 className="ProjectNumber">
-                    {props.projectNumber}
-                </h2>
-                <h1 className="ProjectName">
-                    {props.projectName}
-                </h1>
-            </div>
-         </Link>
-    )
-}
+  return (
+    <Link
+      href="/projects"
+      className="ProjectLabel"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="ProjectsWrapper">
+        <img className="ProjectLogo" src="" alt="">
+          {props.ProjectLogo}
+        </img>
+        <h2 className="ProjectNumber">
+          HypeMind.Inc Drop: {props.projectNumber}
+        </h2>
+        <h1 className="ProjectName">{props.projectName}</h1>
+        <img className="ProjectBackground" src="" alt="">
+          {props.ProjectBackground}
+        </img>
+      </div>
+    </Link>
+  );
+};
 
-export default ProjectContainer
+export default ProjectContainer;
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,17 +1,18 @@
-import ProjectContainer from '../../Components/ProjectContainer/ProjectContainer'
+import ProjectContainer from "../../Components/ProjectContainer/ProjectContainer";
+import "./Projects.css";
 
 const Projects = () => {
-    return (
-        <div className='ProjectsContainer'>
-            <div className='ProjectsRow'>
-                <ProjectContainer projectNumber="#1" projectName="HPMND EXHIBITION"/>
-                <ProjectContainer projectNumber="#1" projectName="PORTFOLIO"/>
-                <ProjectContainer projectNumber="#1" projectName="PORTFOLIO"/>
-                <ProjectContainer projectNumber="#1" projectName="PORTFOLIO"/>
-                <ProjectContainer projectNumber="#1" projectName="PORTFOLIO"/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="ProjectsContainer">
+      <div className="ProjectsRow">
+        <ProjectContainer projectNumber="#1" projectName="HPMND EXHIBITION" />
+        <ProjectContainer projectNumber="#1" projectName="PORTFOLIO" />
+        <ProjectContainer projectNumber="#1" projectName="PORTFOLIO" />
+        <ProjectContainer projectNumber="#1" projectName="PORTFOLIO" />
+        <ProjectContainer projectNumber="#1" projectName="PORTFOLIO" />
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
